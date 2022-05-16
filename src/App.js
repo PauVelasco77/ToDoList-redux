19084887169113bc0626709c4ddb02de5959a789
PauVelasco77 <pau.garrofe@cognativinc.com>
2022-05-16 12:41:13 +0200

    added loadTasks on TaskList component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import Task from "./components/Task/Task";
 import TaskList from "./components/TaskList/TaskList";
-import taskListData from "./data/taskListData";
 
 function App() {
   return (
     <div className="App">
-      <Task task={taskListData[0]} />
-      <TaskList list={taskListData} />
+      <TaskList />
     </div>
   );
 }
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,9 +1,20 @@
 import Task from "../Task/Task";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { loadTasksAction } from "../../redux/actions/actionsCreators";
+import taskListData from "../../data/taskListData";
 
-const TaskList = ({ list }) => {
+const TaskList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadTasksAction(taskListData));
+  }, [dispatch]);
+  const taskList = useSelector((state) => state.taskList);
+
   return (
     <>
-      {list.map((task) => (
+      {taskList.map((task) => (
         <Task key={task.id} task={task} />
       ))}
     </>
