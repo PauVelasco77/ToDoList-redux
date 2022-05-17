88785ddaef42781f4996a44d04820483a2e49ebd
PauVelasco77 <pau.garrofe@cognativinc.com>
2022-05-17 11:17:18 +0200

    created mock folder on data

---
+++ src/data/__mocks__/taskListData.js
@@ -0,0 +1,39 @@
+const taskListData = [
+  {
+    name: "Wash the dishes MOCK",
+    isCompleted: false,
+    date: "September 14, 2016",
+    username: "Leo Messi",
+    id: 1,
+  },
+  {
+    name: "Biscuits",
+    isCompleted: false,
+    date: "September 14, 2016",
+    username: "Cristiano Ronaldo",
+    id: 2,
+  },
+  {
+    name: "Bread",
+    isCompleted: false,
+    date: "September 14, 2016",
+    username: "Shevchenko",
+    id: 3,
+  },
+  {
+    name: "Cheddar cheese",
+    isCompleted: false,
+    date: "September 14, 2016",
+    username: "Andrea Orlandi",
+    id: 4,
+  },
+  {
+    name: "Milk",
+    isCompleted: false,
+    date: "September 14, 2016",
+    username: "Jack Grealish",
+    id: 5,
+  },
+];
+
+export default taskListData;
