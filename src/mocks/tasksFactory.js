23bac252c0da14e44d06a43ff1f2d9fbb9c12375
PauVelasco77 <pau.garrofe@cognativinc.com>
2022-05-17 12:00:04 +0200

    created task factory mock

---
+++ src/mocks/tasksFactory.js
@@ -0,0 +1,13 @@
+import { Factory } from "fishery";
+import faker from "@faker-js/faker";
+
+const taskFactory = Factory.define(() => ({
+  name: faker.lorem.words(3),
+  isCompleted: faker.datatype.boolean(),
+  date: faker.date.soon(),
+  username: faker.name.findName(),
+  id: faker.datatype.number(1000),
+}));
+
+export const generateRandomTask = () => taskFactory.build();
+export const generateRandomTasks = (total) => taskFactory.buildList(total);
