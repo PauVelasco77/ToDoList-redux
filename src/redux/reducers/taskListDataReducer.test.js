af7392bb4b9c0eb7f059403bc8f59c5112ac5299
PauVelasco77 <pau.garrofe@cognativinc.com>
2022-05-17 16:13:55 +0200

    added reducer default case test

--- src/redux/reducers/taskListDataReducer.test.js
+++ src/redux/reducers/taskListDataReducer.test.js
@@ -2,6 +2,14 @@ import { generateRandomTasks } from "../../mocks/tasksFactory";
 import taskListDataReducer from "./taskListDataReducer";
 
 describe("Given a taskListDataReducer reducer", () => {
+  describe("When it doesn't receives any list or action", () => {
+    test("Then it should return an empty list", () => {
+      const newTasks = taskListDataReducer();
+
+      expect(newTasks).toEqual([]);
+    });
+  });
+
   describe("When it receives an action with type loadTasks and a list of tasks", () => {
     test("Then it should return the list of tasks in the in the action", () => {
       const taskList = generateRandomTasks(5);
@@ -19,7 +27,7 @@ describe("Given a taskListDataReducer reducer", () => {
   });
 
   describe("When it receives an action with type isCompletedAction and a task", () => {
-    test("Then it should return the list of tasks in the in the action", () => {
+    test("Then it should return the list of tasks with isCompleted status changed on updated task", () => {
       const taskList = generateRandomTasks(5);
       const task = {
         name: "Wash the dishes",
@@ -29,13 +37,7 @@ describe("Given a taskListDataReducer reducer", () => {
         id: 1,
       };
       taskList.push(task);
-      const updatedTask = {
-        name: "Wash the dishes",
-        isCompleted: true,
-        date: "September 14, 2016",
-        username: "Leo Messi",
-        id: 1,
-      };
+      const updatedTask = { ...task, isCompleted: true };
 
       const action = {
         type: "is-completed",
