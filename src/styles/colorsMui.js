e13a1831bea7c0b629971fb19804eb50714a57b0
PauVelasco77 <pau.garrofe@cognativinc.com>
2022-05-19 10:40:21 +0200

    added random colors from mui

---
+++ src/styles/colorsMui.js
@@ -0,0 +1,74 @@
+import {
+  amber,
+  blue,
+  blueGrey,
+  brown,
+  cyan,
+  deepOrange,
+  deepPurple,
+  green,
+  grey,
+  indigo,
+  lightBlue,
+  lightGreen,
+  lime,
+  orange,
+  pink,
+  purple,
+  red,
+  teal,
+  yellow,
+} from "@mui/material/colors";
+
+export const colorsMui = [
+  "red",
+  "pink",
+  "purple",
+  "deepPurple",
+  "indigo",
+  "blue",
+  "lightBlue",
+  "cyan",
+  "teal",
+  "green",
+  "lightGreen",
+  "lime",
+  "yellow",
+  "amber",
+  "orange",
+  "deepOrange",
+  "brown",
+  "grey",
+  "blueGrey",
+];
+
+export const colorsMui2 = [
+  red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
+  blue,
+  lightBlue,
+  cyan,
+  teal,
+  green,
+  lightGreen,
+  lime,
+  yellow,
+  amber,
+  orange,
+  deepOrange,
+  brown,
+  grey,
+  blueGrey,
+];
+
+export const randomColorsMui = (actualColors) => {
+  const color = colorsMui2[Math.round(Math.random() * (colorsMui2.length - 1))];
+  if (actualColors.some((actualColor) => actualColor === color)) {
+    return randomColorsMui(actualColors);
+  }
+  actualColors.push(color);
+  return color;
+};
