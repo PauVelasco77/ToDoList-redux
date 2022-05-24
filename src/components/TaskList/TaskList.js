ebc8f66482e2fc04fea91fe1b66987644cf7085e
PauVelasco77 <pau.garrofe@cognativinc.com>
2022-05-24 11:41:40 +0200

    added drag&drop on taskList component

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,7 +1,10 @@
 import Task from "../Task/Task";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { loadTasksAction } from "../../redux/actions/actionsCreators";
+import {
+  loadTasksAction,
+  reorderTasksAction,
+} from "../../redux/actions/actionsCreators";
 import taskListData from "../../data/taskListData";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
@@ -9,12 +12,10 @@ import styled from "styled-components";
 const Listul = styled.ul`
   height: 100%;
   width: 100%;
-  background-color: yellow;
 `;
 
 const TaskList = () => {
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(loadTasksAction(taskListData));
   }, [dispatch]);
@@ -22,7 +23,24 @@ const TaskList = () => {
 
   return (
     <>
-      <DragDropContext onDragEnd={(result) => console.log(result)}>
+      <DragDropContext
+        onDragEnd={(result) => {
+          const { source, destination } = result;
+          if (!destination) {
+            return;
+          }
+          if (
+            source.index === destination.index &&
+            source.droppableId === destination.droppableId
+          ) {
+            return;
+          }
+
+          dispatch(
+            reorderTasksAction(result.source.index, result.destination.index)
+          );
+        }}
+      >
         <Droppable droppableId="tasks">
           {(droppableProvided) => (
             <Listul
@@ -32,14 +50,7 @@ const TaskList = () => {
               {taskList.map((task, index) => (
                 <Draggable key={task.id} draggableId={task.name} index={index}>
                   {(draggableProvided) => (
-                    <li
-                      // task={task}
-                      {...draggableProvided.draggableProps}
-                      ref={droppableProvided.innerRef}
-                      {...draggableProvided.dragHandleProps}
-                    >
-                      {task.name}
-                    </li>
+                    <Task task={task} draggableProvided={draggableProvided} />
                   )}
                 </Draggable>
               ))}
