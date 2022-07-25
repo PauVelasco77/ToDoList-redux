f67c2c2ed74c041010dd1b9074b948cb55f3a619
PauVelasco77 <pau.garrofe@cognativinc.com>
2022-07-25 14:16:34 +0200

    added AppContainer in App document

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import "./App.css";
+import styled from "styled-components";
 import TaskForm from "./components/TaskForm/TaskForm";
 import TaskList from "./components/TaskList/TaskList";
 
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <TaskForm />
       <TaskList />
-    </div>
+    </AppContainer>
   );
 }
 
